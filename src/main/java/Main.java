7ba1060cd15efbc512e0fef29c0705303b465d51
RakhimT <127768870+RakhimT@users.noreply.github.com>
2023-07-08 22:42:21 +0300

    Main (3 Task)

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class HelloAppT {
-
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        System.out.println("5 / 6");
-        System.out.println(service.max(5, 6));
-
-    }
-}
