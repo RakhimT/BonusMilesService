d7d9774bc677b9efe81a77366648be7ff87ae812
Rakhim <rakhimturaliev@gmail.com>
2023-07-08 20:42:31 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class HelloAppT {
+
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        System.out.println("5 / 6");
+        System.out.println(service.max(5, 6));
+
+    }
+}
